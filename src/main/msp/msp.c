432e0cd8652fc83f068a0c38f48d24fdc556d9c2
Tao <gongtao0607@gmail.com>
2025-01-22 06:28:39 +0000

    Experimental: scale tail P gain

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1006,12 +1006,14 @@ static bool mspCommonProcessOutCommand(int16_t cmdMSP, sbuf_t *dst, mspPostProce
     }
 
     case MSP_EXPERIMENTAL:
-        /* 
+        /*
          * Send your experimental parameters to LUA. Like:
          *
          * sbufWriteU8(dst, currentPidProfile->yourFancyParameterA);
          * sbufWriteU8(dst, currentPidProfile->yourFancyParameterB);
         */
+        sbufWriteU8(dst, currentPidProfile->p_scale_yaw);
+        sbufWriteU8(dst, currentPidProfile->p_scale_collective);
         break;
 
     default:
@@ -3553,6 +3555,10 @@ static mspResult_e mspCommonProcessInCommand(mspDescriptor_t srcDesc, int16_t cm
          *     currentPidProfile->yourFancyParameterB = sbufReadU8(src);
          * }
         */
+        if (sbufBytesRemaining(src) >= 2) {
+            currentPidProfile->p_scale_yaw = sbufReadU8(src);
+            currentPidProfile->p_scale_collective = sbufReadU8(src);
+        }
         break;
 
     default:
